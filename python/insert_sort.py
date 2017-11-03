bcdad07238a5cf3709b76d9f5421fa96fa201ac6
Oleksii Klymenok <oleksii.klymenok@gmail.com>
2017-11-02 22:46:48 -0700

    [Python] Insert sort

--- python/insert_sort.py
+++ python/insert_sort.py
@@ -1,13 +1,11 @@
-data = [2,3,6,7,8,10,13,5]
+data = [10,4,1,2,5,13,18,6,20]
 
 def insert_sort(data,key_index):
-
 	key = data[key_index]
-	while data[key_index-1]>key:
-		data[key_index]=data[key_index-1]
-		key_index=key_index-1
-	data[key_index]=key
-	return data
+	while data[key_index-1]>key and key_index>0:
+		data[key_index] = data[key_index-1]
+		key_index = key_index - 1
+	data[key_index] = key
 
 for i in range(1,len(data)):
-	data = insert_sort(data,i)
+	insert_sort(data,i)
