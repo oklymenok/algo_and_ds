9e9c199ade0463a79b90323e609621ee67dc2fd1
Oleksii Klymenok <oleksii.klymenok@gmail.com>
2017-12-10 20:26:38 -0800

    [Python] Reverse list recrusive

---
+++ python/reverse_list_recursive.py
@@ -0,0 +1,10 @@
+#!/bin/env
+
+def reverse_list(data):
+	if len(data) == 1:
+		return [data[0]]
+	else:
+		return [data[-1]] + reverse_list(data[0:-1])
+
+data = [1,2,3,4,5,6]
+print reverse_list(data)
